0098459105e06551bb8ba6f4d1ee8173153e5f6a
jaswanth <jaswanthrayavaram07@gmail.com>
2026-05-07 11:51:22 +0530

    virat.py added in master

---
+++ virat.py
@@ -0,0 +1,10 @@
+name="virat"
+team="RCB"
+Name=input("enter player name:")
+Team=input("enter representing team name:")
+details=f"player_name:{name},team:{team}"
+if Name and Team == name and team:
+
+    print(details)
+else:
+    print("details not matching!")
